40e301f564809671c4625ce53c8e8ecc28ed583e
Mikhail248 <misha.sh07@gmail.com>
2026-03-20 18:54:28 +0200

    дата

--- ViewModels/DefaultCakesViewModel.cs
+++ ViewModels/DefaultCakesViewModel.cs
@@ -45,12 +45,13 @@ namespace CakeGrandOrder.ViewModels
         public async Task InitAsync()
         {
             CakeList = new ObservableCollection<Cake>(await AppService.GetInstance().GetCakesAsync());
+
         }
         Order order = new Order();
         public async Task OrderCake(Cake cake)
         {
             order.cakes.Add(cake);
-            AppService.GetInstance().CreateCakeOrder(order);
+            await AppService.GetInstance().CreateCakeOrder(order);
             await Shell.Current.GoToAsync("//CartPage");
         }
         #endregion
