193148d87b2dce4188dc9d8d1bb40892e2160bae
Mikhail248 <misha.sh07@gmail.com>
2026-04-14 23:08:33 +0300

    1

---
+++ Models/RepeatConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeGrandOrder.Models
+{
+    public class RepeatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int count = (int)value;
+
+            return Enumerable.Range(0, count).ToList();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
