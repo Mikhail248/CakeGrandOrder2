6efa0b6a24f36446c9c03728e3cb6b07e2c56d28
Mikhail248 <misha.sh07@gmail.com>
2026-04-18 11:44:10 +0300

    1

--- ViewModels/OrdersViewModel.cs
+++ ViewModels/OrdersViewModel.cs
@@ -16,6 +16,32 @@ namespace CakeGrandOrder.ViewModels
     {
         #region Get Set
         private ObservableCollection<Order> orderList;
+        private ObservableCollection<bool> baseList2;
+        public ObservableCollection<bool> BaseList2
+        {
+            get { return baseList2; }
+            set
+            {
+                if (value != null)
+                {
+                    baseList2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private ObservableCollection<bool> baseList3;
+        public ObservableCollection<bool> BaseList3
+        {
+            get { return baseList3; }
+            set
+            {
+                if (value != null)
+                {
+                    baseList3 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public ObservableCollection<Order> OrderList
         {
             get { return orderList; }
@@ -72,22 +98,40 @@ namespace CakeGrandOrder.ViewModels
         #endregion
 
         #region Methods
-        bool base2 = false;
-        bool base3 = false;
         public async Task InitAsync()
         {
+
             List<Order> tempOrders = new List<Order>(await AppService.GetInstance().GetOrderCakesAsync());
             OrderList = new ObservableCollection<Order>(tempOrders);
-
+            ObservableCollection<bool> tempbases2 = new ObservableCollection<bool>();
+            ObservableCollection<bool> tempbases3 = new ObservableCollection<bool>();
             ObservableCollection<Cake> tempcakes = new ObservableCollection<Cake>();
             for (int i = 0; i < OrderList.Count; i++)
             {
                 for (int j = 0; j < OrderList[i].Cakes.Count; j++)
                 {
                     tempcakes.Add(OrderList[i].Cakes[j]);
+                    if (OrderList[i].Cakes[j].BaseNum >= 2)
+                    {
+                        tempbases2.Add(true);
+                    }
+                    else
+                    {
+                        tempbases2.Add(false);
+                    }
+                    if (OrderList[i].Cakes[j].BaseNum == 3)
+                    {
+                        tempbases3.Add(true);
+                    }
+                    else
+                    {
+                        tempbases3.Add(false);
+                    }
                 }
             }
             CakeList = new ObservableCollection<Cake> (tempcakes);
+            BaseList2 = new ObservableCollection<bool>(tempbases2);
+            BaseList3 = new ObservableCollection<bool>(tempbases3);
         }
         Order order = new Order();
         public async Task OrderCake(Cake cake)
